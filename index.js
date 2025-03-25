2d9db32d5de29920349e0ac14ff15aff8855aa15
Shaaaaaaat <109889828+Shaaaaaaat@users.noreply.github.com>
2025-03-25 21:49:37 +0100

    Ваше описание изменений

--- index.js
+++ index.js
@@ -1123,12 +1123,12 @@ async function sendTwoToAirtable(tgId, invId, sum, lessons, tag, date, nick) {
 }
 
 // Функция для обновления данных в Airtable - clients
-async function sendDateToAirtable(id, date) {
+async function sendDateToAirtable(tgId, date) {
   const apiKey = process.env.AIRTABLE_API_KEY;
   const baseId = process.env.AIRTABLE_BASE_ID;
-  const buyId = process.env.AIRTABLE_CLIENTS_ID;
+  const clientsId = process.env.AIRTABLE_CLIENTS_ID;
 
-  const url = `https://api.airtable.com/v0/${baseId}/${buyId}/${id}`;
+  const url = `https://api.airtable.com/v0/${baseId}/${clientsId}?filterByFormula={tgId}='${tgId}'`;
   const headers = {
     Authorization: `Bearer ${apiKey}`,
     "Content-Type": "application/json",
@@ -1828,7 +1828,7 @@ bot.on("callback_query:data", async (ctx) => {
     const str4 = JSON.parse(str3);
     console.log(`Выбрал дату групповой тренировки - ${str4}`);
 
-    await sendDateToAirtable(session.airtableId, str4);
+    await sendDateToAirtable(ctx.from.id, str4);
   } else if (action.startsWith("later")) {
     console.log("Выбрал позже указать дату групповой тренировки");
     await ctx.reply(
